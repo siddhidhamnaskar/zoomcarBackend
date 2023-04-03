3b96b9816062c66ada35fb6627875ce887e532b8
Siddhi Dhamnaskar <siddhidhamnaskar64@gmail.com>
2023-04-03 06:00:09 +0530

    changes2

--- index.js
+++ index.js
@@ -6,11 +6,13 @@ const connection=require('./config/db')
  const userroute=require("./routes/User")
  const productroute=require("./routes/products")
 const app=express();
-const cors =require("cors");
+const cors=require("cors");
+
 mongoose.set('strictQuery', false);
 app.use(express.json());
 
 
+app.use(cors());
 
 app.use('/',authroute);
 app.use("/user",userroute);
@@ -19,6 +21,8 @@ app.use("/BookingCars",productroute);
 const PORT=3036;
 
 app.listen(PORT,()=>{
+    
+
     try{
         connection();
        console.log("http://localhost:3036");
